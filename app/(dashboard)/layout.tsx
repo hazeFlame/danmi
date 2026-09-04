af26163b1109edc0d97a97dc6ea0b4b9ba5f5c84
hazeflame <myhazeflame@gmail.com>
2026-09-04 16:28:24 +0800

    style: 扩大仪表盘主内容最大宽度并更新本地环境变量示例端口

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -78,7 +78,7 @@ export default function DashboardLayout({
       <div className="flex flex-1 flex-col min-w-0">
         <Header onOpenMobileMenu={() => setMobileMenuOpen(true)} />
 
-        <main className="flex-1 p-4 sm:p-6 lg:p-8 max-w-5xl w-full mx-auto">
+        <main className="flex-1 p-4 sm:p-6 lg:p-8 max-w-8xl w-full mx-auto">
           {children}
         </main>
       </div>
